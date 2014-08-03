98a59e7d94f040e0b9824ba867404a7028baaf88
Thibaud Lopez Schneider <thibaud_lopez@yahoo.com>
2014-08-03 14:12:07 -0700

    Part 1
    Hacking Infor Grid application development (part 1): HelloWorld
    application source (Java entry point) + application deployment
    description (GRID-INF\application.xml) + dynamic web application (JSP) +
    Eclipse project + GAR installable

---
+++ src/net/company/your/HelloWorld.java
@@ -0,0 +1,40 @@
+// javac -cp grid-core-1.11.27.jar net\company\your\HelloWorld.java
+
+package net.company.your;
+
+import com.lawson.grid.node.application.ApplicationEntryPointEx;
+import com.lawson.grid.node.application.ApplicationEntryPointEx.GlobalState;
+import com.lawson.grid.node.application.ApplicationEntryPointEx.RemainingTaskCount;
+import com.lawson.grid.node.application.ModuleContext;
+
+public class HelloWorld implements ApplicationEntryPointEx {
+
+	private boolean isInitialized = false;
+
+	public boolean startModule(ModuleContext paramModuleContext) {
+		return true;
+	}
+
+	public void onlineNotification() {
+		this.isInitialized = true;
+	}
+
+	public void offlineNotification() {
+		this.isInitialized = false;
+	}
+
+	public void stopModule() {
+		this.isInitialized = false;
+	}
+
+	public ApplicationEntryPointEx.RemainingTaskCount getRemainingTaskCount() {
+		return null;
+	}
+
+	public ApplicationEntryPointEx.GlobalState getGlobalState() {
+		if (this.isInitialized) {
+			return new ApplicationEntryPointEx.GlobalState(true, new String[] { "OK" });
+		}
+		return new ApplicationEntryPointEx.GlobalState(false, new String[] { "Initalizing" });
+	}
+}
---
+++ webapps/HelloWorld/index.jsp
@@ -0,0 +1,5 @@
+<%@ page import="java.util.*" %>
+<!DOCTYPE html>
+<html>
+    <body>Hello World! This is my first Grid application. The time is <%=new Date()%></body>
+</html>
