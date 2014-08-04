4bbde00e150eca47f0aa6e04fc7959db6e7be488
Thibaud Lopez Schneider <thibaud_lopez@yahoo.com>
2014-08-03 22:26:28 -0700

    Part 4
    Hacking Infor Grid application development (part 4): write to the Grid
    application log files + read Grid application properties + secure Java
    web application

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ Hacking Infor Grid application development:
 http://thibaudatwork.wordpress.com/2014/07/19/hacking-infor-grid-application-development/
 http://thibaudatwork.wordpress.com/2014/07/25/hacking-infor-grid-application-development-part-2/
 http://thibaudatwork.wordpress.com/2014/07/30/hacking-infor-grid-application-development-part-3/
+http://thibaudatwork.wordpress.com/2014/08/03/hacking-infor-grid-application-development-part-4/
--- src/net/company/your/HelloWorld.java
+++ src/net/company/your/HelloWorld.java
@@ -2,19 +2,34 @@
 
 package net.company.your;
 
+import com.lawson.grid.node.properties.GridProperties;
 import com.lawson.grid.node.application.ApplicationEntryPointEx;
 import com.lawson.grid.node.application.ApplicationEntryPointEx.GlobalState;
 import com.lawson.grid.node.application.ApplicationEntryPointEx.RemainingTaskCount;
 import com.lawson.grid.node.application.ModuleContext;
+import com.lawson.grid.util.logging.GridLogger;
 
 import net.company.your.library1.*;
 
 public class HelloWorld implements ApplicationEntryPointEx {
 
+	private static final GridLogger log = GridLogger.getLogger(HelloWorld.class);
+
 	private boolean isInitialized = false;
 
 	public boolean startModule(ModuleContext paramModuleContext) {
-		System.out.println(HelloWorldLibrary1.getMessage());
+		// use the Grid application library
+		log.info(HelloWorldLibrary1.getMessage());
+		// write to the Grid application log file
+		log.info("Yay, I'm writing to the log file :)");
+		log.warn("Hey, it feels hot in here!");
+		log.error("Ouch, that hurt :(");
+		log.debug("Useful data 48656C6C6F576F726C64");
+		log.trace("Wooohooo 01001000 01100101 01101100 01101100 01101111 00100000 01010111 01101111 01110010 01101100 01100100 00100001");
+		// get a Grid application property
+		GridProperties p = ModuleContext.getCurrent().getProperties();
+		String message = p.getProperty("message");
+		log.info("The message is: " + message);
 		return true;
 	}
 
